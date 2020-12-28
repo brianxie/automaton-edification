eb16c861ba0c01ba15d57c1b3d59451bce4f0bcd
Brian Xie <briancxie@gmail.com>
2020-12-27 21:37:29 -0800

    Add implementation of centroid binary linear classifier

---
+++ src/linear_classifier/centroid_linear_classifier.jl
@@ -0,0 +1,23 @@
+module CentroidLinearClassifier
+
+include("../cluster.jl")
+
+using LinearAlgebra
+
+function model(cs::Array{Float64, 2}, xs::Array{Float64, 2})::Function
+  @assert size(cs, 2) == size(xs, 2)
+  cs_centroid = Cluster.compute_centroid(cs)
+  xs_centroid = Cluster.compute_centroid(xs)
+  
+  normal_vector = cs_centroid .- xs_centroid
+  midpoint = (cs_centroid .+ xs_centroid) ./ 2
+
+  decision_function =
+    function (x)
+      return dot(normal_vector, x) - dot(normal_vector, midpoint)
+    end
+
+  return decision_function
+end
+
+end
---
+++ src/linear_classifier/centroid_linear_classifier_executor.jl
@@ -0,0 +1,37 @@
+using CSV, DataFrames
+
+include("../cluster.jl")
+include("centroid_linear_classifier.jl")
+
+vector_dimension = 3
+positive_size = 100
+negative_size = 100
+
+function write_clusters_to_csv(dimensions::Int64, positive_size::Int64, negative_size::Int64)
+  # Generate two clusters random data points (magic multiplicative factor of 32).
+  positive_center = (rand(Float64, dimensions) .- 0.5) * 32
+  negative_center = (rand(Float64, dimensions) .- 0.5) * 32
+
+  # Generate clusters of points uniformly distributed around each center (magic radius of 1.0).
+  positive_cluster = Cluster.create_uniform_cluster(positive_center, 1.0, positive_size)
+  negative_cluster = Cluster.create_uniform_cluster(negative_center, 1.0, negative_size)
+
+  CSV.write("positive_data.csv", DataFrame(positive_cluster), writeheader=false)
+  CSV.write("negative_data.csv", DataFrame(negative_cluster), writeheader=false)
+
+  return positive_center, negative_center
+end
+
+function read_clusters_from_csv()
+  positive_data = Matrix(CSV.read("positive_data.csv", header=false, DataFrame))
+  negative_data = Matrix(CSV.read("negative_data.csv", header=false, DataFrame))
+
+  return positive_data, negative_data
+end
+
+positive_center, negative_center = write_clusters_to_csv(vector_dimension, positive_size, negative_size)
+positive_data, negative_data = read_clusters_from_csv()
+
+# Train the model.
+# This returns a function that can be applied to a vector of size vector_dimension to classify.
+model = CentroidLinearClassifier.model(positive_data, negative_data)
