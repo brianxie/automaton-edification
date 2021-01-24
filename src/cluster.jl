a6ac1b56bf9b7a11d999f59e94dca8f683aeab6f
Brian Xie <briancxie@gmail.com>
2021-01-23 16:07:02 -0800

    Reformat source files according to Julia conventions

--- src/cluster.jl
+++ src/cluster.jl
@@ -3,21 +3,24 @@ module Cluster
 using LinearAlgebra
 
 # Returns a num_points * size(center) array of vectors
-function create_uniform_cluster(center::Array{Float64, 1}, perturbation::Float64, num_points::Int64)
-  ones_vec = ones(Float64, num_points, 1)
-  reshaped_center = transpose(reshape(center, size(center, 1), 1))
+function create_uniform_cluster(center::Array{Float64, 1},
+                                perturbation::Float64,
+                                num_points::Int64)
+    ones_vec = ones(Float64, num_points, 1)
+    reshaped_center = transpose(reshape(center, size(center, 1), 1))
 
-  # num_points * 1, 1 * size(center) => num_points copies of center
-  center_vecs = ones_vec * reshaped_center
+    # num_points * 1, 1 * size(center) => num_points copies of center
+    center_vecs = ones_vec * reshaped_center
 
-  random_perturbations = (rand(Float64, num_points, size(center, 1), ) .- 0.5) * 2
+    random_perturbations =
+        (rand(Float64, num_points, size(center, 1), ) .- 0.5) * 2
 
-  return center_vecs + random_perturbations
+    return center_vecs + random_perturbations
 end
 
 function compute_centroid(points::Array{Float64, 2})::Array{Float64, 1}
-  point_sum = sum(points, dims=1)[:]
-  return point_sum ./ size(points, 1)
+    point_sum = sum(points, dims=1)[:]
+    return point_sum ./ size(points, 1)
 end
 
 end
--- src/linear_classifier/centroid_linear_classifier_executor.jl
+++ src/linear_classifier/centroid_linear_classifier_executor.jl
@@ -7,29 +7,41 @@ vector_dimension = 3
 positive_size = 100
 negative_size = 100
 
-function write_clusters_to_csv(dimensions::Int64, positive_size::Int64, negative_size::Int64)
-  # Generate two clusters random data points (magic multiplicative factor of 32).
-  positive_center = (rand(Float64, dimensions) .- 0.5) * 32
-  negative_center = (rand(Float64, dimensions) .- 0.5) * 32
+function write_clusters_to_csv(dimensions::Int64,
+                               positive_size::Int64,
+                               negative_size::Int64)
+    # Generate two clusters random data points (magic multiplicative factor of
+    # 32).
+    positive_center = (rand(Float64, dimensions) .- 0.5) * 32
+    negative_center = (rand(Float64, dimensions) .- 0.5) * 32
 
-  # Generate clusters of points uniformly distributed around each center (magic radius of 1.0).
-  positive_cluster = Cluster.create_uniform_cluster(positive_center, 1.0, positive_size)
-  negative_cluster = Cluster.create_uniform_cluster(negative_center, 1.0, negative_size)
+    # Generate clusters of points uniformly distributed around each center
+    # (magic radius of 1.0).
 
-  CSV.write("positive_data.csv", DataFrame(positive_cluster), writeheader=false)
-  CSV.write("negative_data.csv", DataFrame(negative_cluster), writeheader=false)
+    positive_cluster =
+        Cluster.create_uniform_cluster(positive_center, 1.0, positive_size)
+    negative_cluster =
+        Cluster.create_uniform_cluster(negative_center, 1.0, negative_size)
 
-  return positive_center, negative_center
+    CSV.write("positive_data.csv",
+              DataFrame(positive_cluster), writeheader=false)
+    CSV.write("negative_data.csv",
+              DataFrame(negative_cluster), writeheader=false)
+
+    return positive_center, negative_center
 end
 
 function read_clusters_from_csv()
-  positive_data = Matrix(CSV.read("positive_data.csv", header=false, DataFrame))
-  negative_data = Matrix(CSV.read("negative_data.csv", header=false, DataFrame))
+    positive_data =
+        Matrix(CSV.read("positive_data.csv", header=false, DataFrame))
+    negative_data =
+        Matrix(CSV.read("negative_data.csv", header=false, DataFrame))
 
-  return positive_data, negative_data
+    return positive_data, negative_data
 end
 
-positive_center, negative_center = write_clusters_to_csv(vector_dimension, positive_size, negative_size)
+positive_center, negative_center =
+    write_clusters_to_csv(vector_dimension, positive_size, negative_size)
 positive_data, negative_data = read_clusters_from_csv()
 
 positive_centroid = Cluster.compute_centroid(positive_data)
